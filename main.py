c6c8f7a52d3c7e9cd72131b8757d9f36666b3bd0
Ethan Meideck <ethan@meideck.net>
2025-08-17 19:32:05 +0200

    Adding ProximaProject class and iss_tracking function with docstrings

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello World")
+from time import sleep
+import requests
+
+class ProximaProject:
+    """
+    Main application class for the ProximaProject.
+    This class provides all the functions to run the application. 
+
+    Notes:
+        This class relies on 'time' and 'requests' libraries.
+    """
+
+    def iss_tracking(self):
+        """
+        Track the International Space Station with it's latitude and longitude.
+
+            Return:
+                    coordinates (list): Current ISS coordinates as [latitude, longitude].
+        
+        """
+        pass
+
+if __name__ == "__main__":
+    p = ProximaProject()
+    print(p.iss_tracking.__doc__)
