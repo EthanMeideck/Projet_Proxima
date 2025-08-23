d3f044427e31bf78bfbd6519357dcc6a1d94d771
Ethan Meideck <ethan@meideck.net>
2025-08-23 20:04:47 +0200

    Feat: Implement ISS tracking function.
    Fetching ISS location and store coordinates in variables.

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ class ProximaProject:
         This class relies on 'time' and 'requests' libraries.
     """
 
+    ISS_TRACKING_URL = "http://api.open-notify.org/iss-now.json"
+
     def iss_tracking(self):
         """
         Track the International Space Station with it's latitude and longitude.
 
             Return:
-                    coordinates (list): Current ISS coordinates as [latitude, longitude].
+                    Coordinates (list): Current ISS coordinates as [latitude, longitude].
         
         """
-        pass
+
+        iss_tracking_response = requests.get(self.ISS_TRACKING_URL)
+        
+        if iss_tracking_response.status_code == 200:
+            iss_tracking_data = iss_tracking_response.json()
+            self.iss_longitude = iss_tracking_data["iss_position"]["longitude"]
+            self.iss_latitude = iss_tracking_data["iss_position"]["longitude"]
+
+        else:
+            print(f"Error {iss_tracking_response.status_code}")
+            
 
 if __name__ == "__main__":
     p = ProximaProject()
-    print(p.iss_tracking.__doc__)
+    print(p.iss_tracking())
